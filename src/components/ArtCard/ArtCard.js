3263ab750b85e962dbff436c4e40e5fefd9bd2f4
Pragyansh Chaturvedi (r41k0u) <pragyanshchaturvedi18@gmail.com>
2021-11-04 11:56:13 +0530

    Fixed Cards

--- src/components/ArtCard/ArtCard.js
+++ src/components/ArtCard/ArtCard.js
@@ -3,15 +3,17 @@ import "./ArtCard.css";
 import image from '../../img/download.jpeg'
 const ArtCard = () => {
   return (
+    <div className="card-container">
       <div className="card" >
         <div className="imgBx">
-        <img src={image} alt=""/>
+          <img src={image} alt="" />
         </div>
         <div className="content">
-          <h2> NFT CARD</h2>
+          <p className="title">NFT CARD</p>
           <p>Lorem ipsum dolor sit amet,orem ipsum dolor sit amet, consectetur adipiscing elit,orem ipsum dolor sit amet, </p>
         </div>
       </div>
+    </div>
   );
 };
 
